4698dfac4853f0229c7a19fda2804ae7a00b06ba
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2024-06-10 16:16:24 -0300

    iniciando projeto

---
+++ main/main.mjs
@@ -0,0 +1,46 @@
+import { app, BrowserWindow, session } from 'electron'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const createWindow = async () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+
+  if (app.isPackaged) {
+    const { default: serve } = await import('electron-serve')
+    const appServe = serve({ directory: path.join(__dirname, '../out') })
+    appServe(win).then(() => {
+      win.loadURL('app://-')
+    })
+  } else {
+    win.loadURL('http://localhost:3000')
+    win.webContents.openDevTools()
+    win.webContents.on('did-fail-load', () => {
+      win.webContents.reloadIgnoringCache()
+    })
+  }
+}
+
+app.on('ready', async () => {
+  const cookie = {
+    url: 'http://localhost:3000',
+    name: 'application',
+    value: 'on-electron',
+  }
+  await session.defaultSession.cookies.set(cookie)
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
