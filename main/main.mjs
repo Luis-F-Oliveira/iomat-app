23c28f1dfe9d8c98f1c85a47ee5cafccaebc66d0
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2024-06-12 16:44:21 -0300

    adicionando path

--- main/main.mjs
+++ main/main.mjs
@@ -1,6 +1,7 @@
 import { app, BrowserWindow, session } from 'electron'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
+import path from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
